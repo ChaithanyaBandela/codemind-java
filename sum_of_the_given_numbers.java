de42de78d534099d08a3bc93cc3b82f734c4d403
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-08-29 21:54:32 +0530

    import java.util.*; public class sum{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),i;         for(i=0;i<n;i++)         {             int a=sc.nextInt(),b=sc.nextInt();             System.out.println(a+b);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),i;
+        for(i=0;i<n;i++)
+        {
+            int a=sc.nextInt(),b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
