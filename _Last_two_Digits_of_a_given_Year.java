0714ec7063299022b63616acf7ea1995d2e677b3
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-10-21 21:54:34 +0530

    import java.util.*; public class LastDigit{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.printf("%02d",n%100);     } }

---
+++ _Last_two_Digits_of_a_given_Year.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class LastDigit{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.printf("%02d",n%100);
+    }
+}
