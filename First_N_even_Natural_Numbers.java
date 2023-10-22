a060edf000a198c2dfa829686ac14e8bf3513d68
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-10-22 22:17:31 +0530

    import java.util.*; public class Natural{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=n*2;i>0;i--)         {             if(i%2==0)             {                 System.out.print(i+" ");             }         }     } }

---
+++ First_N_even_Natural_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Natural{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n*2;i>0;i--)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
