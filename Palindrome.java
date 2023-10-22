38a9dccc5ce62ec328d8171e0f4fbe3677a35fad
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-10-22 21:38:55 +0530

    import java.util.*; public class Palindrome{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i=n;         int rev=0;         while(n!=0)         {             int rem=n%10;             rev=rev*10+rem;             n=n/10;         }         if(i==rev)         {             System.out.println("Palindrome");         }         else{             System.out.println("Not Palindrome");         }     } }

--- Palindrome.java
+++ Palindrome.java
@@ -1,23 +1,23 @@
 import java.util.*;
-public class pal{
+public class Palindrome{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int i;
-        i=n;
-        int rem,re=0;
-        while(n>0)
+        int i=n;
+        int rev=0;
+        while(n!=0)
         {
-            rem=n%10;
-            re=re*10+rem;
+            int rem=n%10;
+            rev=rev*10+rem;
             n=n/10;
         }
-        if(re==i){
-            System.out.print("True");
+        if(i==rev)
+        {
+            System.out.println("Palindrome");
         }
         else{
-            System.out.print("False");
+            System.out.println("Not Palindrome");
         }
     }
 }
