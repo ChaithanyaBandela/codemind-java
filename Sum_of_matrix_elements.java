2cafbaddd79761b227f0026e616029d0bbf42355
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-10-28 12:55:32 +0530

    import java.util.*; public class SumOfMatrix{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int m=sc.nextInt(),n=sc.nextInt();         int[][] arr=new int[m][n];         for(int i=0;i<m;i++)         {             for(int j=0;j<n;j++)             {                 arr[i][j]=sc.nextInt();             }         }         int s=0;         for(int i=0;i<m;i++)         {             for(int j=0;j<n;j++)             {                 s+=arr[i][j];             }         }         System.out.println(s);     } }

--- Sum_of_matrix_elements.java
+++ Sum_of_matrix_elements.java
@@ -1,23 +1,25 @@
 import java.util.*;
-public class mat{
+public class SumOfMatrix{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int R=sc.nextInt(),C=sc.nextInt(),i,j;
-        int[][] arr=new int[R][C];
-        for(i=0;i<R;i++)
+        int m=sc.nextInt(),n=sc.nextInt();
+        int[][] arr=new int[m][n];
+        for(int i=0;i<m;i++)
         {
-            for(j=0;j<C;j++)
+            for(int j=0;j<n;j++)
             {
                 arr[i][j]=sc.nextInt();
             }
         }
-        int su=0;
-        for(i=0;i<R;i++){
-            for(j=0;j<C;j++){
-                su+=arr[i][j];
+        int s=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                s+=arr[i][j];
             }
         }
-        System.out.print(su);
+        System.out.println(s);
     }
 }
